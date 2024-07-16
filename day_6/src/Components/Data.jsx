b96703ebb53cd797f1c200e5ec79e6759b851cb6
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-07-16 13:09:29 +0530

    Product data

---
+++ day_6/src/Components/Data.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+function Data() {
+  
+    const [state,setState]=useState(false)
+    const [dataa,setData]=useState([])
+    
+
+    
+    useEffect(()=>{
+        fetch(`https://fakestoreapi.com/products`)
+        .then((res)=>res.json())
+        .then((res)=>{
+            setData(res)
+            console.log(res)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[])
+    
+    console.log(dataa);
+
+return (
+    <div style={{display:"flex"}}>
+        {
+            dataa.map((el)=>{
+                
+                return (
+                    <div>
+                        <img src={el.image}  style={{width:"250px",height:"250px", display:"flex", justifyContent:"space-evenly"}}/>
+                        <span>{el.title}</span>
+                        <h5>Price:{el.price}</h5>
+                    </div>
+                    )
+            })
+        }
+        <button onClick={()=>setState(!state)}>Fetch data</button>
+    </div>
+  )
+}
+
+
+export default Data

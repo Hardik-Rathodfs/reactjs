5457a84b815bc849228e310078f2822e3b99132c
Hardik Rathod <157630585+HARDIK-RATHOD-9899@users.noreply.github.com>
2024-09-04 12:27:04 +0530

    My_First_reactapp

---
+++ day_01/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <form>
+      <h1>Name : Hardik rathod</h1>
+      <h1>Course : Full-Stack</h1>
+      <h1>Age : 22</h1>
+      <h1>Mobile No: 6353123475</h1>
+     </form>
+    </div>
+  );
+}
+
+export default App;
